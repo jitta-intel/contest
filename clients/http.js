f3f3ab661eebdd2f8fa3497473a20ce1481d1ac6
Manoch Changsalak <benhatira@gmail.com>
2021-01-15 10:42:32 +0700

    support many type of http response

--- clients/http.js
+++ clients/http.js
@@ -5,8 +5,12 @@ const test = async (uri) => {
   // Make a request for a user with a given ID
   try {
     const result = await axios.get(uri, {
-      timeout: 3000
+      timeout: 3000,
+      headers: {
+        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+      }
     })
+    
     if (result.status === 200) {
       return {
         pass: true
---
+++ demo/graph-cms.js
@@ -0,0 +1,14 @@
+const Contester = require('../index')
+
+
+const contester = new Contester([
+  {
+    name: 'graph cloudfunctions',
+    uri: ''
+  }, {
+    name: 'graphcms',
+    uri: ''
+  }
+])
+
+contester.run()
